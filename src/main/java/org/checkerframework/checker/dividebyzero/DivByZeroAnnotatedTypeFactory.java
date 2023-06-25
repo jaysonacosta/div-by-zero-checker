7e0abc2db21fcef43fe5138b1ce2c3082b2e32f3
Jayson Acosta <36345453+jaysonacosta@users.noreply.github.com>
2023-06-24 23:54:17 -0400

    defaultAnnotation returns lattice point given a literal

--- src/main/java/org/checkerframework/checker/dividebyzero/DivByZeroAnnotatedTypeFactory.java
+++ src/main/java/org/checkerframework/checker/dividebyzero/DivByZeroAnnotatedTypeFactory.java
@@ -23,19 +23,23 @@ public class DivByZeroAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
     /**
      * Compute the default annotation for the given literal.
      *
-     * @param literal   the literal in the syntax tree to examine
+     * @param literal the literal in the syntax tree to examine
      * @return the most specific possible point in the lattice for the given literal
      */
     private Class<? extends Annotation> defaultAnnotation(LiteralTree literal) {
         switch (literal.getKind()) {
-        case INT_LITERAL:
-            int intValue = (Integer)literal.getValue();
-            // TODO
-            break;
-        case LONG_LITERAL:
-            long longValue = (Long)literal.getValue();
-            // TODO
-            break;
+            case INT_LITERAL:
+                int intValue = (Integer) literal.getValue();
+                if (intValue == 0) {
+                    return Zero.class;
+                }
+                return NonZero.class;
+            case LONG_LITERAL:
+                long longValue = (Long) literal.getValue();
+                if (longValue == 0) {
+                    return Zero.class;
+                }
+                return NonZero.class;
         }
         return Top.class;
     }
@@ -51,8 +55,8 @@ public class DivByZeroAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
     @Override
     protected TreeAnnotator createTreeAnnotator() {
         return new ListTreeAnnotator(
-            new DivByZeroTreeAnnotator(this),
-            super.createTreeAnnotator());
+                new DivByZeroTreeAnnotator(this),
+                super.createTreeAnnotator());
     }
 
     private class DivByZeroTreeAnnotator extends TreeAnnotator {
@@ -74,14 +78,14 @@ public class DivByZeroAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
 
         // From Suzanne Millstein, 2017/05/08:
         //
-        //  > The AnnotatedTypeFactory only applies types computed by dataflow
-        //  > if they are a subtype of the type it computed.  By default the
-        //  > type of a binary tree is the lub of the two operands.  (In your
-        //  > example, that means the type of "1-1" as computed by the type
-        //  > factory is @NonZero which is not a super type of @Zero, so it is
-        //  > discarded.)
+        // > The AnnotatedTypeFactory only applies types computed by dataflow
+        // > if they are a subtype of the type it computed. By default the
+        // > type of a binary tree is the lub of the two operands. (In your
+        // > example, that means the type of "1-1" as computed by the type
+        // > factory is @NonZero which is not a super type of @Zero, so it is
+        // > discarded.)
         //
-        // The example she referenced is "int x = 1 / (1 - 1)".  So, to get the
+        // The example she referenced is "int x = 1 / (1 - 1)". So, to get the
         // transfer rules to work properly for complex expressions like that
         // one, we must override the "output-is-lub-of-operands" behavior. By
         // default, everything should be Top.
